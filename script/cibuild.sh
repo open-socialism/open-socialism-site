ba1aeaf269a63d16307c00e1ffc438470b92765f
Alistair Burrowes <alistair.burrowes@gmail.com>
2016-03-04 16:30:02 +1100

    Update cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,2 +1,10 @@
-gem install
-jekyll build && htmlproof ./_site
+#!/usr/bin/env bash
+set -e # halt script on error
+
+JEKYLLBUILDOUTPUT="$(bundle exec jekyll build)"
+echo "${JEKYLLBUILDOUTPUT}"
+if echo "${JEKYLLBUILDOUTPUT}" | grep -i -q "ERROR"; then
+    printf '%s\n' 'An Error was found in the Jekyll build' >&2
+    exit 1
+fi
+bundle exec 'htmlproof ./_site --href_ignore "#"'
