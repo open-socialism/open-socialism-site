4e07d90429e21a5abefb5c00ada3d66e9ccb6ebf
Alistair Burrowes <alistair.burrowes@gmail.com>
2016-05-11 17:05:53 +1000

    more travis attempts

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-JEKYLLBUILDOUTPUT="$(bundle exec jekyll build --source .)"
+JEKYLLBUILDOUTPUT="$(bundle exec jekyll build)"
 echo "${JEKYLLBUILDOUTPUT}"
 if echo "${JEKYLLBUILDOUTPUT}" | grep -i -q "ERROR"; then
     printf '%s\n' 'An Error was found in the Jekyll build' >&2
