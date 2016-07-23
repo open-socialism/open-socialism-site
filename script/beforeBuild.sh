684af568b2fb5f9a7e966e8dec65a2e47945310c
Alistair Burrowes <alistair.burrowes@gmail.com>
2016-07-23 16:03:09 +1000

    remove old ci build stuff

--- script/beforeBuild.sh
+++
@@ -1,7 +0,0 @@
-# set git stuff
-git init
-git config user.name "travis ci"
-git config user.email "travis@travis-ci.org"
-
-# update submodules
-./script/updateSubmodules.sh
--- script/deploy.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "Updated submodules"
-
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:test > /dev/null 2>&1
